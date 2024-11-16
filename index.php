649ac86609eb7d2ab4898e987af1599ed6c5ec7a
Masirika Constantin <elconstant73@gmail.com>
2024-11-16 23:29:18 +0100

    premier fichier

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis earum fugit voluptatum mollitia nesciunt nam voluptas accusamus, laborum, dolores laboriosam tempora veritatis soluta iure, veniam doloribus vitae accusantium culpa totam?
+    
+</body>
+</html>
